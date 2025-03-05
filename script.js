3432e29411ee7b50d1017c9b6f94bf56980a9469
satyam jha <109567398+satyamjha09@users.noreply.github.com>
2025-03-05 12:46:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 function highlight() {
-    //Write your code here
-
-
+    document.querySelectorAll('strong').forEach(element => {
+        element.style.color = 'rgb(0, 128, 0)';
+    });
 }
 
+function return_normal() {
+    document.querySelectorAll('strong').forEach(element => {
+        element.style.color = 'rgb(0, 0, 0)';
+    });
+}
 
 function return_normal() {
     //Write your code here
